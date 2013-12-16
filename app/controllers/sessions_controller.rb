65d59181be28943df062c9565a35e2b55f5a7dc9
amrita <amritasharma20jan@gmail.com>
2013-12-16 16:14:16 +0530

    session change

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,12 +5,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-  	# if user = User.authenticate(params[:name], params[:password])
-   #     session[:user_id] = user.id
-   #     redirect_to admin_url
-   #  else
-   #     redirect_to login_url, :alert => "Invalid user/password combination"
-   #  end
+  	if user = User.authenticate(params[:name], params[:password])
+       session[:user_id] = user.id
+       redirect_to admin_url
+    else
+       redirect_to login_url, :alert => "Invalid user/password combination"
+    end
 
   end
 
